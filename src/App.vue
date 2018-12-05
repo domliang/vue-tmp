36846eebf5bd543737260e8bd195877bd26de37b
dom <dom@boo.chat>
2018-12-05 15:47:11 +0800

    add simplest store

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <a-menu
+      v-model="current"
+      mode="horizontal"
+    >
+      <a-menu-item key="home">
+        <router-link to="/">Home</router-link>
+      </a-menu-item>
+      <a-menu-item key="about">
+        <router-link to="/about">About</router-link>
+      </a-menu-item>
+    </a-menu>
+    <div class="pages">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.pages {
+  padding-top: 10px;
   text-align: center;
-  color: #2c3e50;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      current: ['home']
     }
   }
 }
-</style>
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
     <img width="200" alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your H2O Vue.js App"/>
+    <h1>{{ count }}</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { mapState } from 'vuex'
+import { setInterval } from 'timers'
+
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  computed: {
+    ...mapState({
+      count: state => state.count
+    })
+  },
+  mounted () {
+    setInterval(() => {
+      this.$store.dispatch({
+        type: 'increment'
+      })
+    }, 1000)
   }
 }
 </script>
