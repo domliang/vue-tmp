9e25d021ef550c68b8e09f2e936a252e8add42f3
dom <dom@boo.chat>
2018-12-05 17:17:45 +0800

    add pwa

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'ant-design-vue/dist/antd.css'
 import App from './App.vue'
 import router from './router'
 import store from './store/index'
+import './registerServiceWorker'
 
 Vue.config.productionTip = false
 
